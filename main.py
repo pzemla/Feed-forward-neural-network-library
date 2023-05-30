eb443b32038fdc779590e7d9d82402ca3dc466db
pzemla <patryk.zemla.stud@pw.edu.pl>
2023-05-31 00:33:06 +0200

    Minor changes to code

--- main.py
+++ main.py
@@ -9,16 +9,12 @@ from DataLoader import *
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 from matplotlib import pyplot as plt
-from math import sqrt
 
-#size - 216(treningowe)+54(testowe)=270
 
-#preprocessing
-txt = open(r'C:\Users\karol\Desktop\SNB\Statlog_(Heart)_MLR\heart.dat')
+#Example neural network data preprocessing (predicting hearth disease based on data)
+txt = open(r'heart.dat')
 txt_list = [line.replace('\n','').split(' ') for line in txt.readlines()]
 df = pd.DataFrame(txt_list, columns=['age','sex','chestpaintype','resting blood pressure', 'serum cholestoral', 'fasting blood sugar', 'restingelectrocardiographicresults', 'maximum heart rate achieved', 'exercise induced angina', 'oldpeak', 'slope of the peak exercise', 'number of major vessels', 'thal', 'heartdisease'])
-
-#ONEHOT
 ohe = OneHotEncoder()
 feature_arry = ohe.fit_transform(df[["chestpaintype","restingelectrocardiographicresults","thal"]]).toarray()
 feature_labels=np.array(["chestpaintype_1","chestpaintype_2","chestpaintype_3","chestpaintype_4","restingelectrocardiographicresults_1","restingelectrocardiographicresults_2","restingelectrocardiographicresults_3","thal_1","thal_2","thal_3"])
@@ -40,9 +36,9 @@ test=test.astype(float)
 train=train.astype(float)
 minimum=train.min()
 maximum=train.max()
-train=((train-minimum)/(maximum-minimum)) #normalizacja
+train=((train-minimum)/(maximum-minimum))
 
-test=((test-minimum)/(maximum-minimum)) #normalizacja
+test=((test-minimum)/(maximum-minimum))
 train['heartdisease']=heartdisease
 
 test['heartdisease']=heartdisease_test
@@ -56,7 +52,7 @@ train[:,-1]-=1
 test[:,-1]-=1
 
 
-#Ustalanie parametrów sieci neuoronowej 
+#Neural network parameters
 epochs = 50
 batch_size_val=1
 
@@ -94,8 +90,6 @@ for epoch in range(epochs):
     net.evaluation_mode()
     for inputs,labels in test_loader:
         eval = np.round(net.evaluate(inputs))
-        #print("EVAL",len(eval))
-        #print("LABELS",labels)
         if (eval == labels):
             if(labels==1):
                 TP+=1
@@ -106,44 +100,30 @@ for epoch in range(epochs):
                 FN += 1
             elif (labels == 0):
                 FP += 1
-
-    #zrobić arraye,appendować do nich i potem zrobić plot
-    #te dwa mają być w sprawku
-    sensitivity=TP/(TP+FN) #czułość
-    specificity=TN/(FP+TN) #specyficzność
-    print(sensitivity)
-    print(specificity)
-
-    #dodatkowe miary, ewentualnie można dodać
-    false_positive_rate=FP/(FP+TN) #częstość fałszywych alarmów
-    false_discovery_rate=FP/(FP+TP) #częstość fałszywych odkryć
-
-    positive_precision=TP/(TP+FP) #precyzja pozytywna(jedynek-obecności choroby)
-    negative_precision=TN/(TN+FN) #precyzja negatywna(zer-braku obecności choroby)
-
-    accuracy=(TP+TN)/(TP+TN+FP+TN) #dokładność
-
-    f1_score=(2*TP)/(2*TP+FP+FN) #średnia harmoniczna z precyzji i czułości
-
-    MCC=(TP*TN-FP*FN)/(sqrt((TP+FP)*(TP+FN)*(TN+FP)*(TN+FN))) #współczynnik korelacji matthews (matthews correlation coefficient)
-
-    #To się może przydać, bo mamy inną wagę fałszywych jedynek i zer w opisie zadania
-    beta=5 #waga źle sklasyfikowanych jedynek ma być 5 razy większa niż źle sklasyfikowanych zer, jeśli dobrze pamiętam
-    fb_score=(1+5)*(positive_precision*sensitivity)/((1+5)*positive_precision+sensitivity)
-
-
-
-
     TP_array.append(TP)
     TN_array.append(TN)
     FP_array.append(FP)
     FN_array.append(FN)
 
 
-#WYKRESY STRAT, DOKŁADNOŚCI ITD.
-#length = [i for i in range(len(TP_array))]
+#Various indicators for true/false neural network
+# sensitivity=TP_array/(TP_array+FN_array)
+# specificity=TN_array/(FP_array+TN_array)
+# false_positive_rate=FP_array/(FP_array+TN_array)
+# false_discovery_rate=FP_array/(FP_array+TP_array)
+# positive_precision=TP_array/(TP_array+FP_array)
+# negative_precision=TN_array/(TN_array+FN_array)
+# accuracy=(TP_array+TN_array)/(TP_array+TN_array+FP_array+FN_array)
+# f1_score=(2*TP_array)/(2*TP_array+FP_array+FN_array)
+# MCC=(TP_array*TN_array-FP_array*FN_array)/(sqrt((TP_array+FP_array)*(TP_array+FN_array)*(TN_array+FP_array)*(TN_array+FN_array)))
+# beta=5
+# fb_score=(1+5)*(positive_precision*sensitivity)/((1+5)*positive_precision+sensitivity)
+
+#Charts
 length = [i for i in range(len(loss_array))]
 plt.plot(length, loss_array)
+
+#length = [i for i in range(len(TP_array))]
 #plt.plot(length, TP_array, 'r', length, TN_array, 'b', length, FP_array, 'g',length, FN_array, 'k')
 #plt.legend(["TP","TN","FP","FN"])
 plt.show()
